7efb407d077ffdea374b073e0248aaac1f453ff6
Devilin <bogdan.lev@icloud.com>
2025-07-29 20:22:39 +0100

    1 Year of historical data instead of 7 days

--- Cryptocurrency/Cryptocurrency/Core/Detail/View/ChartView.swift
+++ Cryptocurrency/Cryptocurrency/Core/Detail/View/ChartView.swift
@@ -34,8 +34,8 @@ struct ChartView: View {
         let priceChange = (data.last ?? 0) - (data.first ?? 0)
         lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
         
-        endingDate = Date(coinGeckoString: coin.lastUpdated ?? "")
-        startingDate = endingDate.addingTimeInterval(-7*24*60*60)
+        endingDate = Date(coinGeckoString: coin.lastUpdated ?? "") ?? Date()
+        startingDate = endingDate.addingTimeInterval(-365*24*60*60) // Changed from -7 to -365 days
     }
     
     var body: some View {
